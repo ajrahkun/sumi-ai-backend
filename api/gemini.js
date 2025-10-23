28dc9708a999d0696eebe321923618a55430e483
ajrahkun <haiakuazraa@gmail.com>
2025-10-23 16:37:38 +0700

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
       return res.status(400).json({ text: "Prompt is required." });
     }
 
-    const genAI = new GoogleGenerativeAI("AIzaSyCiSPocyE7lragSeEhke6relCxqxiXYvvI");
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
     const result = await model.generateContent(prompt);
 
